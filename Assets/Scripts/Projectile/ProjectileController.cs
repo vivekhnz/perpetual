66736a6fd1a45361051d08426048b3d1dc7662f9
Vivek Hari <vivek.hari@outlook.co.nz>
2017-04-13 18:07:49 +1000

    Move projectile in the direction it is pointing.

---
+++ Assets/Scripts/Projectile/ProjectileController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ProjectileController : MonoBehaviour {
+
+	public float MovementSpeed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(
+			Vector3.right * Time.deltaTime * MovementSpeed);
+	}
+}
