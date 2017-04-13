abcb4e19954daf853a708f34f2694415e85a6c77
Vivek Hari <vivek.hari@outlook.co.nz>
2017-04-13 17:57:10 +1000

    Rotate to face mouse cursor.

---
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // calculate world position of mouse cursor
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var plane = new Plane(Vector3.forward, Vector3.zero);
+        float distance = 0.0f;
+        if (plane.Raycast(ray, out distance))
+        {
+            Vector3 mousePos = ray.GetPoint(distance);
+
+            // calculate rotation based on direction to mouse cursor
+            Vector3 dirToMouse = mousePos - transform.position;
+            float rotation = Mathf.Atan2(dirToMouse.y, dirToMouse.x)
+                * Mathf.Rad2Deg;
+
+			// set the player rotation
+			transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotation);
+        }
+    }
+}
