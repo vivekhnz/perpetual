a88c04c245466e26a21c2aa57f6262ea177fac04
Vivek Hari <vivek.hari@outlook.co.nz>
2017-04-23 04:12:35 +1000

    Added particle effect explosion when projectile is destroyed.

--- Assets/Scripts/Projectile/ProjectileController.cs
+++ Assets/Scripts/Projectile/ProjectileController.cs
@@ -4,13 +4,14 @@ public class ProjectileController : PooledObject
 {
     public float MovementSpeed = 4.0f;
     public float Damage = 10.0f;
+    public ParticleSystem ProjectileExplosion;
 
     public void Initialize(Vector3 position, Quaternion rotation)
     {
         transform.position = position;
         transform.rotation = rotation;
     }
-	
+
     void FixedUpdate()
     {
         transform.Translate(
@@ -24,6 +25,7 @@ public class ProjectileController : PooledObject
             case "Solid":
                 // destroy this projectile
                 Recycle();
+                CreateExplosion();
                 break;
             case "Damageable":
                 // damage the object
@@ -33,7 +35,24 @@ public class ProjectileController : PooledObject
 
                 // destroy this projectile
                 Recycle();
+                CreateExplosion();
                 break;
         }
+    }
+
+    private void CreateExplosion()
+    {
+        var explosion = Instantiate(ProjectileExplosion);
+        explosion.transform.position = transform.position;
+        
+        float angle = transform.rotation.eulerAngles.z + 180.0f;
+        Vector2 direction = new Vector2(
+            Mathf.Cos(angle * Mathf.Deg2Rad),
+            Mathf.Sin(angle * Mathf.Deg2Rad));
+
+        var velocity = explosion.velocityOverLifetime;
+        velocity.enabled = true;
+        velocity.x = new ParticleSystem.MinMaxCurve(direction.x * 3.0f);
+        velocity.y = new ParticleSystem.MinMaxCurve(direction.y * 3.0f);
     }
 }
