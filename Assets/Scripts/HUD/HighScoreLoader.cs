975a7d75ccf490598869ab2f983fc7ea914cf274
NATHANTAN\Nathaniel <natanjtan@gmail.com>
2017-04-23 19:50:03 +1000

    Added highscore function and text to gameplay and title scenes

---
+++ Assets/Scripts/HUD/HighScoreLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreLoader : MonoBehaviour {
+
+    public Text HighScoreText;
+
+    private int hiScore;
+
+	// Use this for initialization
+	void Start () {
+        hiScore = 0;
+
+		if (PlayerPrefs.HasKey("HighScore"))
+        {
+            hiScore = PlayerPrefs.GetInt("HighScore");
+        }
+
+        HighScoreText.text = "High Score: " + hiScore;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
