6e8983f042fa97d98aa9107bb656bc1855aa4df3
Guan Du <platinumgamer@hotmail.com>
2017-05-03 21:44:29 +1000

    Added knockback from the explosion effect and included easy to understand comments.

--- Assets/BlastDamage.cs
+++ Assets/BlastDamage.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class BlastDamage : MonoBehaviour {
 
-    public float blastDamage;
+    public float blastDamage; // Damage of the explosion.
     public float playerDamageModifier; // The damage modifier applied to the player. Normally < 1.
+    public float blastForce; // Determines how powerful the blast will push other objects.
+
+    private float blastRadius;
+
+    void Start()
+    {
+        // Obtain the blast radius from the radius of the circle collider for consistency.
+        blastRadius = GetComponent<CircleCollider2D>().radius;
+    }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
@@ -23,9 +32,45 @@ public class BlastDamage : MonoBehaviour {
                 // Get the player's health script and damage the player.
                 PlayerHealth playerHeath = collider.GetComponentInChildren<PlayerHealth>();
                 playerHeath.TakeDamage(blastDamage * playerDamageModifier);
-
                 break;
-
         }
+
+        // Knockback the objects after damage has been dealt.
+        // Early mobs will die instantly to explosion, so it mainly affects player.
+        KnockBackObjects();
+    }
+
+    // Knocks back all damageable objects.
+    void KnockBackObjects()
+    {
+        // Get origin of explosion to prepare for vector calculations.
+        Vector3 explosionPos = transform.position;
+
+        // Get ALL colliders that are in the blast radius.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPos, blastRadius);
+
+        // Apply blast force to all colliding GameObjects that have Rigidbodies.
+        foreach (Collider2D hit in colliders)
+        {
+            Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                AddExplosionForce(rb, blastForce, explosionPos, blastRadius);
+            }
+        }
+    }
+
+    // Creates an explosive force that moves a rigidbody away from the source of the explosion.
+    private void AddExplosionForce(Rigidbody2D body, float explosionForce, Vector3 explosionPosition, float explosionRadius)
+    {
+        // Calculate the vector of the explosion by subtracting the explosion's position from the target position.
+        var dir = (body.transform.position - explosionPosition);
+
+        // Calculate a proportional decrease in power the further away the target is from the explosion.
+        float wearoff = 1 - (dir.magnitude / explosionRadius);
+
+        // Add the force to the rigidbody as an Impulse force for immediate effect.
+        body.AddForce(dir.normalized * explosionForce * wearoff, ForceMode2D.Impulse);
     }
 }
