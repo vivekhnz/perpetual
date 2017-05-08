14d647e0d92ff7181dd521d9c3bc609de9fe2571
Vivek Hari <vivek.hari@outlook.co.nz>
2017-05-08 15:23:06 +1000

    Render laser beam.

--- Assets/Scripts/Weapons/LaserWeapon.cs
+++ Assets/Scripts/Weapons/LaserWeapon.cs
@@ -1,36 +1,48 @@
 ﻿using UnityEngine;
 using System.Linq;
 
+[RequireComponent(typeof(LineRenderer))]
 public class LaserWeapon : MonoBehaviour
 {
     public float Damage = 10.0f;
 
+    LineRenderer line;
     int layerMask;
 
     void Start()
     {
+        line = GetComponent<LineRenderer>();
+        if (line == null)
+            Debug.LogError("No line renderer found!");
+
         layerMask = LayerMask.GetMask("Default", "Obstacles");
     }
 
     void FixedUpdate()
     {
+        line.enabled = false;
+
         // is the laser being fired?
         if (Input.GetButton("FireSecondary"))
         {
             // perform raycast
-            var dirToMouse =
-                Camera.main.ScreenToWorldPoint(Input.mousePosition)
-                - transform.position;
+            var mousePos = Camera.main.ScreenToWorldPoint(
+                Input.mousePosition);
+            var dirToMouse = mousePos - transform.position;
             var hits = Physics2D.RaycastAll(
                 transform.position, dirToMouse.normalized,
                 float.MaxValue, layerMask);
+            Vector2 laserEnd = transform.position;
             foreach (var hit in hits.OrderBy(h => h.distance))
             {
                 var tag = hit.collider.gameObject.tag;
 
                 // have we hit a wall?
                 if (tag.Equals("Solid"))
+                {
+                    laserEnd = hit.point;
                     break;
+                }
 
                 // have we hit a damageable object?
                 if (tag.Equals("Damageable"))
@@ -43,6 +55,11 @@ public class LaserWeapon : MonoBehaviour
                             transform.rotation.eulerAngles.z);
                 }
             }
+
+            // draw laser
+            line.enabled = true;
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, laserEnd);
         }
     }
 }
