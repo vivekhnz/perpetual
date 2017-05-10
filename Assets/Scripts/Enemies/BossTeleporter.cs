d8dab04ca56c7f63616ff89380900766c1177cf2
NATHANTAN\Nathaniel <natanjtan@gmail.com>
2017-05-10 13:12:39 +1000

    Added teleport script to boss, teleports to random location

---
+++ Assets/Scripts/Enemies/BossTeleporter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BossTeleporter : MonoBehaviour {
+
+    public float timer = 5;
+
+    private List<Vector3> teleportLocations;
+    private float teleportTime;
+    
+	void Start () {
+
+        // initialise timeholder
+        teleportTime = Time.time + 10;
+        
+        // find teleport points
+        teleportLocations = GameObject.FindGameObjectsWithTag("TeleportPoint")
+            .Select(obj => obj.transform.position).ToList();
+        if (teleportLocations.Count == 0)
+            Debug.LogError("No teleport points defined!");
+
+    }
+	
+	void FixedUpdate () {
+		if ((Time.time - teleportTime) > timer)
+        {
+            transform.position = teleportLocations[Random.Range(0, teleportLocations.Count)];
+            teleportTime = Time.time;
+        }
+	}
+}
