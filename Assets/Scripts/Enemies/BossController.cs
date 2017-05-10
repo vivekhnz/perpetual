6aab22950f7b0c6d43bc79f816b445bd5f792c02
NATHANTAN\Nathaniel <natanjtan@gmail.com>
2017-05-10 22:12:11 +1000

    Added Boss weapon, created boss projectiles

--- Assets/Scripts/Enemies/BossController.cs
+++ Assets/Scripts/Enemies/BossController.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class BossTeleporter : MonoBehaviour {
+public class BossController : MonoBehaviour {
 
     public float TimeToTeleport = 5;
     public float TimeToHide = 3;
+    public float MovementSpeed = 2.0f;
+    public BossProjectileController Projectile;
+    public float ProjectileSpreadDegrees = 10.0f;
+    public float BulletsPerMinute = 60.0f;
+    public Transform weapon;
 
     private List<Vector3> teleportLocations;
     private float teleportTime;
@@ -14,8 +19,12 @@ public class BossTeleporter : MonoBehaviour {
     private Vector3 hidingSpot;
     private bool hiding;
     private ChasePlayer movement;
-    
-	void Start () {
+    private EnemyController controller;
+    private float projectileFiredTime = 0.0f;
+
+    void Start () {
+
+        controller = GetComponent<EnemyController>();
 
         // get movement
         movement = GetComponent<ChasePlayer>();
@@ -36,8 +45,19 @@ public class BossTeleporter : MonoBehaviour {
 	
 	void FixedUpdate () {
 
+        if (controller.Player == null)
+            return;
+
+        Fire();
+
+        // rotate to face player
+        Vector2 direction = controller.Player.transform.position - transform.position;
+        direction.Normalize();
+        transform.rotation = Quaternion.Euler(0, 0,
+            Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+        
         // if time to teleport, teleport boss
-		if (!hiding && (Time.time - teleportTime) > TimeToTeleport)
+        if (!hiding && (Time.time - teleportTime) > TimeToTeleport)
         {
             hiding = true;
             transform.position = hidingSpot;
@@ -55,5 +75,26 @@ public class BossTeleporter : MonoBehaviour {
                 teleportTime = Time.time;
             }
         }
+    }
+
+    void Fire()
+    {
+        if (Projectile == null)
+            return;
+
+        // can I fire again?
+        float timeBetweenProjectiles = 60.0f / BulletsPerMinute;
+        if (Time.time - projectileFiredTime < timeBetweenProjectiles)
+            return;
+        projectileFiredTime = Time.time;
+
+        // calculate projectile direction
+        float projectileDir = transform.rotation.eulerAngles.z
+            + Random.Range(-ProjectileSpreadDegrees, ProjectileSpreadDegrees);
+
+        // spawn projectile
+        var projectile = Projectile.Fetch<BossProjectileController>();
+        projectile.Initialize(
+            weapon.position, Quaternion.Euler(0.0f, 0.0f, projectileDir));
     }
 }
