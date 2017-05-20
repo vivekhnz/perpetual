0b032092d7b62ff4e5542f037c0ae67799779a9c
Vivek Hari <vivek.hari@outlook.co.nz>
2017-05-21 03:13:24 +1000

    Adjusted cooldowns.

--- Assets/Scripts/Player/Abilities/NukeAbility.cs
+++ Assets/Scripts/Player/Abilities/NukeAbility.cs
@@ -1,30 +1,27 @@
 ﻿using System;
 using UnityEngine;
 
-public class NukeAbility : PlayerAbility {
+public class NukeAbility : PlayerAbility
+{
+    public float Cooldown = 12;
 
-    public float Cooldown = 3;
-    public NukeController NukeController;
-
+    private NukeController controller;
     private float nukeTime;
 
     void Start()
     {
-        NukeController = GetComponent<NukeController>();
+        controller = GetComponent<NukeController>();
     }
 
     void FixedUpdate()
     {
         if (Input.GetButton("Ability") && Time.time - nukeTime > Cooldown)
-        {
             Nuke();
-        }
     }
 
     private void Nuke()
     {
-        NukeController.CreateShockwave();
-
+        controller.CreateShockwave();
         nukeTime = Time.time;
     }
 
