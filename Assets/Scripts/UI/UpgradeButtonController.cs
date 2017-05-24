527ff43681a4ac7556ee2bbbd01867de04bf70d1
Vivek Hari <vivek.hari@outlook.co.nz>
2017-05-25 00:17:04 +1000

    Moved upgrade buttons into new prefab.

---
+++ Assets/Scripts/UI/UpgradeButtonController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class UpgradeButtonController : MonoBehaviour
+{
+    public string UpgradeName;
+    public string UpgradeType;
+    public Sprite UpgradeIcon;
+
+    public Text UpgradeNameText;
+    public Text UpgradeTypeText;
+    public Image UpgradeButtonIcon;
+    public UnityEvent OnUpgradeSelected;
+
+    void Start()
+    {
+        UpgradeNameText.text = UpgradeName;
+        UpgradeTypeText.text = UpgradeType;
+        UpgradeButtonIcon.sprite = UpgradeIcon;
+    }
+
+    public void UpgradeSelected()
+    {
+        if (OnUpgradeSelected != null)
+            OnUpgradeSelected.Invoke();
+    }
+}
