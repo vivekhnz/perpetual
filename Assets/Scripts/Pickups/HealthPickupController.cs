296b69a37a3e8ccb0f0a9cac4d818bb2310b3718
Guan Du <platinumgamer@hotmail.com>
2017-05-26 22:26:20 +1000

    Added flashing animation effect 3 seconds before despawn

--- Assets/Scripts/Pickups/HealthPickupController.cs
+++ Assets/Scripts/Pickups/HealthPickupController.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 public class HealthPickupController : MonoBehaviour {
 
     public float HealthGained = 30;
-    public float ActiveTime = 5;
+    public float TimeTilDespawn = 5;
+    public float TimeBeforeDespawnToFlash = 3;
     public AudioClip HealthPickedUpSfx;
+
+    private Animator animator;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
+
         // health pickup will disappear after set time
-        Invoke("Disappear", ActiveTime);
+        Invoke("Disappear", TimeTilDespawn);
+        Invoke("StartFlashing", TimeTilDespawn - TimeBeforeDespawnToFlash);
     }
 
     void Disappear()
     {
         Destroy(gameObject);
+    }
+
+    void StartFlashing()
+    {
+        // makes the sprite flash on and off just before despawn
+        animator.SetBool("IsDespawning", true);
     }
 
     void OnTriggerEnter2D(Collider2D other)
