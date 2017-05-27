a99a5cbaeead00ad55e5aa4f8f506d28ea6a6871
NATHANTAN\Nathaniel <natanjtan@gmail.com>
2017-05-28 00:11:02 +1000

    Add NukeCooldown mod

---
+++ Assets/Scripts/Player/Upgrades/Mods/NukeCoolDownMod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class NukeCoolDownMod : AbilityModUpgrade<NukeAbility> {
+
+    public float Cooldown = 9.0f;
+
+    protected override void ApplyMod(NukeAbility ability)
+    {
+        ability.Cooldown = Cooldown;
+    }
+
+}
