1cd9dbea57c0088cd95fef22b3ceb44108e1c93e
NATHANTAN\Nathaniel <natanjtan@gmail.com>
2017-05-27 10:36:24 +1000

    Added MaxHealthMod

---
+++ Assets/Scripts/Player/Upgrades/Mods/MaxHealthMod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class MaxHealthMod : PlayerModUpgrade {
+
+    public float MaximumHealth = 150.0f;
+
+    protected override void ApplyMod(GameObject player)
+    {
+        var health = GetHealth(player);
+        health.InitialHealth = MaximumHealth;
+    }
+
+}
