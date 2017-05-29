d8a7d452759625e6cc5b012184f7b84b2f1a2740
NATHANTAN\Nathaniel <natanjtan@gmail.com>
2017-05-29 22:13:45 +1000

    Add ScatterShot Range mod

---
+++ Assets/Scripts/Player/Upgrades/Mods/ScattershotRangeMod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScattershotRangeMod : WeaponModUpgrade<ScatterShotWeapon> {
+
+    public float ProjectileRange = 1.0f;
+
+    protected override void ApplyMod(ScatterShotWeapon weapon)
+    {
+        weapon.ProjectileRange = ProjectileRange;
+    }
+
+}
