84e015b0e14ad3d949f1c9c810378b142839c7f2
NATHANTAN\Nathaniel <natanjtan@gmail.com>
2017-05-29 22:22:18 +1000

    Added scattershot projectiles amount mod

---
+++ Assets/Scripts/Player/Upgrades/Mods/ScatterShotProjectilesMod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScatterShotProjectilesMod : WeaponModUpgrade<ScatterShotWeapon> {
+
+    public int ProjectileAmount = 15;
+
+    protected override void ApplyMod(ScatterShotWeapon weapon)
+    {
+        weapon.ProjectileAmount = ProjectileAmount;
+    }
+}
