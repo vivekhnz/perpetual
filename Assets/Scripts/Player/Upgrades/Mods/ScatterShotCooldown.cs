40382630b4f7edb03b0844fbc1c4bb424f248e61
NATHANTAN\Nathaniel <natanjtan@gmail.com>
2017-05-29 22:18:15 +1000

    Added ScatterShot Cooldown mod

---
+++ Assets/Scripts/Player/Upgrades/Mods/ScatterShotCooldown.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScatterShotCooldown : WeaponModUpgrade<ScatterShotWeapon> {
+
+    public float Cooldown = 0.5f;
+
+    protected override void ApplyMod(ScatterShotWeapon weapon)
+    {
+        weapon.Cooldown = Cooldown;
+    }
+}
